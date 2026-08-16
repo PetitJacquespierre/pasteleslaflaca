692bc3298203da8481c215c3efd5ee48ec0e892c
PetitJacquespierre <petitjacquespierre@gmail.com>
2026-08-16 13:15:27 -0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuración Maestra del Cliente
 const clientConfig = {
     businessName: "Pasteles La Flaca",
-    whatsapp: "584120000000", // Reemplazar con el real
+    whatsapp: "584120609841", // Reemplazar con el real
     colors: {
         primary: "#F59E0B",   // Naranja Ámbar de La Flaca
         bgDark: "#09090b",    // Negro Carbón
